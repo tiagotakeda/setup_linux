f728ad9fd0006d4ba3eeda1313b472c5a753d5c8
tiagotakeda <tiagotakeda@usp.br>
2021-08-23 12:25:06 -0300

    awscli eksctl

--- setup_ubuntu20_wsl2_darede.sh
+++ setup_ubuntu20_wsl2_darede.sh
@@ -62,3 +62,10 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 grep -E --color 'vmx|svm' /proc/cpuinfo
+
+## EKSCTL
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+## AWSCLI
+sudo apt install awscli -y
